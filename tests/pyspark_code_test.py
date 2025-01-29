5bd7fbe667ccd903cd71c952ac120556adcf92d0
Gyogeshwar <52628334+Gyogeshwar@users.noreply.github.com>
2025-01-29 17:05:45 +0530

    Update pyspark_code_test.py (#13)

--- tests/pyspark_code_test.py
+++ tests/pyspark_code_test.py
@@ -5,5 +5,5 @@ from pyspark.sql.functions import col
 spark = SparkSession.builder.appName("Pyspark unit test").getOrCreate()
 
 df1 = spark.createDataFrame(data=[("1", 1000), ("2", 3000)], schema=["id", "amount"])
-df2 = spark.createDataFrame(data=[("1", 1000), ("2", 3000), ("3", 4000)], schema=["id", "amount"])
+df2 = spark.createDataFrame(data=[("1", 1000), ("2", 3000)], schema=["id", "amount"])
 assert sorted(df1.collect()) == sorted(df2.collect())
