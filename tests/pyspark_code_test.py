9b61604ca88a4f62343deb3b07a4a9a78fb8cea2
Gyogeshwar <52628334+Gyogeshwar@users.noreply.github.com>
2025-01-30 18:17:41 +0530

    Update pyspark_code_test.py

--- tests/pyspark_code_test.py
+++ tests/pyspark_code_test.py
@@ -7,3 +7,6 @@ spark = SparkSession.builder.appName("Pyspark unit test").getOrCreate()
 df1 = spark.createDataFrame(data=[("1", 1000), ("2", 3000)], schema=["id", "amount"])
 df2 = spark.createDataFrame(data=[("1", 1000), ("2", 3000)], schema=["id", "amount"])
 assert sorted(df1.collect()) == sorted(df2.collect())
+
+df1.write.csv("result/expected/df1.csv", header=True)
+df2.write.csv("result/output/df2.csv", header=True)
