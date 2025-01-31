084a968031326e328e98a273222b56c924c2f5be
Gyogeshwar <52628334+Gyogeshwar@users.noreply.github.com>
2025-01-31 18:15:59 +0530

    Update pyspark_code_test.py
    updated code

--- tests/pyspark_code_test.py
+++ tests/pyspark_code_test.py
@@ -2,11 +2,24 @@ import pytest
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 
-spark = SparkSession.builder.appName("Pyspark unit test").getOrCreate()
+def create_spark_session():
+  spark = SparkSession.builder.appName("Pyspark unit test").getOrCreate()
+  return spark
 
-df1 = spark.createDataFrame(data=[("1", 1000), ("2", 3000)], schema=["id", "amount"])
-df2 = spark.createDataFrame(data=[("1", 1000), ("2", 3000)], schema=["id", "amount"])
-assert sorted(df1.collect()) == sorted(df2.collect())
+def create_data_frames(spark):
+  df1 = spark.createDataFrame(data=[("1", 1000), ("2", 3000)], schema=["id", "amount"])
+  df2 = spark.createDataFrame(data=[("1", 1000), ("2", 3000)], schema=["id", "amount"])
+  return df1, df2
 
-df1.write.csv("result/expected/df1.csv", header=True)
-df2.write.csv("result/output/df2.csv", header=True)
+def compare_dataframes_publish(spark, df1, df2):
+  assert sorted(df1.collect()) == sorted(df2.collect())
+
+
+def main():
+  spark = create_spark_session()
+  df1, df2 = create_data_frames(spark)
+  compare_dataframes_publish(spark, df1, df2)
+  df1.write.csv("result/expected/df1.csv", header=True)
+  df2.write.csv("result/output/df2.csv", header=True)
+  
+ 
