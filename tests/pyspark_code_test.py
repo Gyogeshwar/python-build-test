8a88d34d6dc8cfe0c5036340d5be3232970d598e
Gyogeshwar <52628334+Gyogeshwar@users.noreply.github.com>
2025-02-05 19:34:21 +0530

    Update pyspark_code_test.py

--- tests/pyspark_code_test.py
+++ tests/pyspark_code_test.py
@@ -20,16 +20,6 @@ def data_frames(spark):
 def compare_dataframes_publish(df1, df2):
     """Compare two DataFrames."""
     assert sorted(df1.collect()) == sorted(df2.collect())
-
-def test_compare_dataframes(spark, data_frames):
-    """Test case for comparing DataFrames."""
-    df1, df2 = data_frames
-    compare_dataframes_publish(df1, df2)
-
-def main():
-    spark = create_spark_session()
-    df1, df2 = create_data_frames(spark)
-    
     workspace = os.environ.get("GITHUB_WORKSPACE", "/home/runner/work/python-build-test/python-build-test/")
 
     # Ensure output directories exist
@@ -40,6 +30,16 @@ def main():
     df1.write.csv(os.path.join(workspace, "result/expected/df1.csv"), header=True)
     df2.write.csv(os.path.join(workspace, "result/output/df2.csv"), header=True)
 
+
+def test_compare_dataframes(spark, data_frames):
+    """Test case for comparing DataFrames."""
+    df1, df2 = data_frames
+    compare_dataframes_publish(df1, df2)
+
+def main():
+    spark = create_spark_session()
+    df1, df2 = create_data_frames(spark)
+    
     # Compare DataFrames
     compare_dataframes_publish(df1, df2)
 
