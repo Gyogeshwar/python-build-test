428c9110c796e59f05e6a58cac731b560b0f56cb
Yogeshwar Gadade <gadadeyogeshwar@gmail.com>
2025-02-09 18:05:39 +0530

    Updated documantation & pytest code

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # python-build-test
-Created this repository for test the python build.
+Created this repository for unit testing of python code created for data transformation.
+
+## To run the unit test follow the below commands
+
+1. Install Required library
+
+```shell
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+```
+
+2. Run unit test 
+
+```shell
+pytest -v
+```
+
--- tests/pyspark_code_test.py
+++ tests/pyspark_code_test.py
@@ -2,6 +2,7 @@ import pytest
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 import os
+import shutil
 
 @pytest.fixture(scope="module")
 def spark():
@@ -15,6 +16,8 @@ def data_frames(spark):
     """Create sample DataFrames for testing."""
     df1 = spark.createDataFrame(data=[("1", 1000), ("2", 3000)], schema=["id", "amount"])
     df2 = spark.createDataFrame(data=[("1", 1000), ("2", 3000)], schema=["id", "amount"])
+    df1.show()
+    df2.show()
     return df1, df2
 
 def compare_dataframes_publish(df1, df2):
@@ -23,8 +26,13 @@ def compare_dataframes_publish(df1, df2):
     workspace = os.environ.get("GITHUB_WORKSPACE", "/home/runner/work/python-build-test/python-build-test/")
 
     # Ensure output directories exist
-    os.makedirs(os.path.join(workspace, "result/expected"), exist_ok=True)
-    os.makedirs(os.path.join(workspace, "result/output"), exist_ok=True)
+    if os.path.exists(os.path.join(workspace, "result/")):
+        shutil.rmtree(os.path.join(workspace, "result/"))
+        print("The folder has been deleted successfully!")
+    else:
+        print("Can not delete the folder as it doesn't exists")
+        os.makedirs(os.path.join(workspace, "result/expected"), exist_ok=True)
+        os.makedirs(os.path.join(workspace, "result/output"), exist_ok=True)
     
     # Write DataFrames to CSV
     df1.coalesce(1).write.mode("overwrite").csv(os.path.join(workspace, "result/expected/df1.csv"), header=True)
@@ -37,8 +45,8 @@ def test_compare_dataframes(spark, data_frames):
     compare_dataframes_publish(df1, df2)
 
 def main():
-    spark = create_spark_session()
-    df1, df2 = create_data_frames(spark)
+    spark = spark()
+    df1, df2 = data_frames(spark)
     
     # Compare DataFrames
     compare_dataframes_publish(df1, df2)
